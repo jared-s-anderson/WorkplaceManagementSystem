4c1e9ae664e0bb356521602915cc3e5fb2afb3a5
jaredsanderson <jaredsa@byui.edu>
2022-10-31 22:20:41 -0600

    Controllers and Views Updated

--- Controllers/RewardsController.cs
+++ Controllers/RewardsController.cs
@@ -18,5 +18,98 @@ namespace WorkplaceManagementSystem.Controllers
             IEnumerable<Rewards> rewards = _db.Rewards;
             return View(rewards);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Rewards rewardObject)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Rewards.Add(rewardObject);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rewardObject);
+        }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var rewardFromDatabase = _db.Rewards.Find(id);
+
+            if (rewardFromDatabase == null)
+            {
+                return NotFound();
+            }
+
+            return View(rewardFromDatabase);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Rewards rewardObject)
+        {
+            if (rewardObject.Reward == rewardObject.RewardDescription)
+            {
+                ModelState.AddModelError("Custom Error", "The reward name cannot be the same as the reward description.");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Rewards.Update(rewardObject);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rewardObject);
+        }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var rewardFromDatabase = _db.Rewards.Find(id);
+
+            if (rewardFromDatabase == null)
+            {
+                return NotFound();
+            }
+
+            return View(rewardFromDatabase);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var rewardFromDatabase = _db.Rewards.Find(id);
+            if (rewardFromDatabase == null)
+            {
+                return NotFound();
+            }
+
+            _db.Rewards.Remove(rewardFromDatabase);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
