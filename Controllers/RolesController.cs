b36d28e91e505dd7502041b33df470d6ef9a540c
jaredsanderson <jaredsa@byui.edu>
2022-11-08 20:15:22 -0700

    Started Roles MVC

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using WorkplaceManagementSystem.Data;
+using WorkplaceManagementSystem.Models;
+
+namespace WorkplaceManagementSystem.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        //private readonly ApplicationDbContext _db;
+
+       // public RolesController(RoleManager<IdentityRole> roleManager, ApplicationDbContext db) : this(roleManager)
+        //{
+       //     _db = db;
+       // }
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new()
+                {
+                    Name = model.RoleName
+                };
+
+                var result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkplaceManagementSystem.Models
+{
+    public class RoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
