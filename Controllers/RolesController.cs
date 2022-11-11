2d9c059990874be8e5d421711c663e5552bf9e7d
jaredsanderson <jaredsa@byui.edu>
2022-11-10 20:19:29 -0700

    Roles Controller Updated

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -10,10 +10,12 @@ namespace WorkplaceManagementSystem.Controllers
     public class RolesController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public RolesController(RoleManager<IdentityRole> roleManager)
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [Authorize(Roles = "Administrator, Manager")]
@@ -52,5 +54,58 @@ namespace WorkplaceManagementSystem.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(string? id)
+        {
+            var roles = await _roleManager.FindByIdAsync(id);
+
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            EditRolesViewModel model = new()
+            {
+                Id = roles.Id,
+                RoleName = roles.Name,
+            };
+
+
+            foreach (var user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, roles.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditRolesViewModel model)
+        {
+            var roles = await _roleManager.FindByIdAsync(model.Id);
+
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                roles.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(roles);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
---
+++ Models/EditRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkplaceManagementSystem.Models
+{
+    public class EditRolesViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [DisplayName("Role Name")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; } = new();
+    }
+}
