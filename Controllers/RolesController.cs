96906bce6829cb9fb96c93c0dc08b2cdfa1a0c47
jaredsanderson <jaredsa@byui.edu>
2022-11-11 22:58:39 -0700

    Role Management Added

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using System.Data;
 using WorkplaceManagementSystem.Data;
 using WorkplaceManagementSystem.Models;
@@ -106,6 +107,79 @@ namespace WorkplaceManagementSystem.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUserRoles(string? id)
+        {
+            var roles = await _roleManager.FindByIdAsync(id);
+
+            ViewData["roleId"] = id;
+            ViewData["roleName"] = roles.Name;
+
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            var model = new List<UserRolesModel>();
+
+            foreach (var user in _userManager.Users)
+            {
+                UserRolesModel userRoles = new()
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                };
+
+                if (await _userManager.IsInRoleAsync(user, roles.Name))
+                {
+                    userRoles.IsSelected = true;
+                }
+
+                else
+                {
+                    userRoles.IsSelected = false;
+                }
+
+                model.Add(userRoles);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUserRoles(List<UserRolesModel> model, string? id)
+        {
+            var roles = await _roleManager.FindByIdAsync(id);
+
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                var users = await _userManager.FindByIdAsync(model[i].UserId);
+
+                if (model[i].IsSelected && !(await _userManager.IsInRoleAsync(users, roles.Name)))
+                {
+                    await _userManager.AddToRoleAsync(users, roles.Name);
+                }
+
+                else if (!model[i].IsSelected && await _userManager.IsInRoleAsync(users, roles.Name))
+                {
+                    await _userManager.RemoveFromRoleAsync(users, roles.Name);
+                }
+                
+                else
+                {
+                    continue;
+                }
+            }
+
+            return RedirectToAction("Index");
+
         }
     }
 }
