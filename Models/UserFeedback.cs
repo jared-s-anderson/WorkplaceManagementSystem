30e442750a70f7c0519552b087290d977fd8a3d5
jaredsanderson <jaredsa@byui.edu>
2022-11-29 20:59:56 -0700

    User Feedback Option Added

---
+++ Models/UserFeedback.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkplaceManagementSystem.Models
+{
+    public class UserFeedback
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Feedback { get; set; }
+    }
+}
